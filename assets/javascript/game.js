1520707427c4c96a785b8f9789e00463a958e418
Victoire <victoirebaron@hotmail.fr>
2019-05-21 22:27:02 -0700

    Replacing  random word by dashes and correction of key press listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,44 @@
+var winningPoint = document.getElementById("winning-point");
+var currentWord = document.getElementById("current-word");
+var numberGuesses = document.getElementById("number-guesses");
+
+var wins = 0;
+var number = 0;
+var lettersGuesses = [];
+
 // List of electro bands
-const words = ["Disclosure","Guetta","Walker","Daftpunk", "Justice", "Diplo"];
+const words = ["Disclosure", "Guetta", "Walker", "Daftpunk", "Justice", "Diplo"];
 
 // Pick a random word
-const word = words[Math.floor(Math.random() * words.length)];
-const wordLower = word.toLowerCase();
-console.log(word)
+const computerWord = words[Math.floor(Math.random() * words.length)];
 
-// on key press 
-document.addEventListener("keypress", (event) => {
-    const letter = event.key;
-    const isInWord = wordLower.includes(letter); 
-   console.log(isInWord)
-})
+// replace the random word by dashes
+for (i = 0; i < computerWord.length; i++) {
+    currentWord.innerHTML = currentWord.innerHTML + "_ "
+}
+
+// on key press listener
+document.onkeypress = (event) => {
+    const userKey = event.key.toLowerCase();
+    const isInWord = computerWord.toLowerCase().includes(userKey);
+
+
+    if (isInWord) {
+        // document.getElementById("user-Key").textContent = userKey;
+        
+    } else {
+        if (lettersGuesses.includes(userKey) == false) {
+            lettersGuesses.push(userKey);
+            document.getElementById("letters-guesses").textContent = lettersGuesses;
+        }
+    }
+}
+
+    // if(isInWord /* && letter not already guessed */){
+    //     // display letter in currentWord
+    //     const letterGuess 
+    //     var currentWord
+    // } else if(/* is the letter not already guessed */){
+    //     // display letter in lettersGuesses
+    //     // decrease -1 number of guesses remaining except
+    // }
