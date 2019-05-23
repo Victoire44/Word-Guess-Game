3692e7c11c4fe688acfec0d7d9647d3531800729
Victoire <victoirebaron@hotmail.fr>
2019-05-22 20:58:23 -0700

    creation of two functions for the score and the guesses left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,48 @@
-var guessesLeft = document.getElementById("guessesLeft");
-
 var score = 0;
-var guessesLeft = 0;
+var guessesLeft = 13;
 var lettersGuesses = [];
 var currentWord = [];
 
 // List of electro bands
-const words = ["Disclosure", "Overwerk", "Walker", "Daftpunk", "Justice", "Diplo"];
+const words = ["Disclosure", "Overwerk", "AlanWalker", "Daftpunk", "Justice", "Diplo", "Davidguetta"];
 
 // Pick a random word
 const computerWord = words[Math.floor(Math.random() * words.length)];
 
-// replace the random word by dashes
+// Replace the random word by dashes
 for (i = 0; i < computerWord.length; i++) {
     currentWord.push("_")   
 }
 document.getElementById("current-word").textContent = currentWord.join(" ");
 
+// Functions
+function updateScore() {
+    document.getElementById("score").innerHTML = "Wins: " + score;
+}
+updateScore();
+
+function updateGuesses() {
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+}
+updateGuesses();
+
+
 // on key press listener
-document.onkeyup = (event) => {
+document.onkeyup = function (event){
     const userKey = event.key.toUpperCase();
     const letterIndex = computerWord.toLocaleUpperCase().indexOf(userKey);
 
     if (letterIndex !== -1) {
         currentWord.splice(letterIndex, 1, userKey);
         document.getElementById("current-word").textContent = currentWord.join(" ");
+        score++;
+        updateScore();
 
     } else if (lettersGuesses.includes(userKey) == false) {
         lettersGuesses.push(userKey);
         document.getElementById("letters-guesses").textContent = lettersGuesses.join(" ").toUpperCase();
+        guessesLeft--;
+        updateGuesses();
     }
 }
 
-    // if(isInWord /* && letter not already guessed */){
-    //     // display letter in currentWord
-    //     const letterGuess 
-    //     var currentWord
-    // } else if(/* is the letter not already guessed */){
-    //     // display letter in lettersGuesses
-    //     // decrease -1 number of guesses remaining except
-    // }
